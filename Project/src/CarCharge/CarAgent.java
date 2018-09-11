a1ab51d4056d6dcf11f49013f05a37e613c8c8f6
Eliza <elizabeth.wain@hotmail.com>
2018-09-11 17:24:22 +1000

    no message

--- Project/src/CarCharge/CarAgent.java
+++ Project/src/CarCharge/CarAgent.java
@@ -1,6 +1,11 @@
 package CarCharge;
 
+import java.util.Iterator;
+
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.core.AID;
 
 //a basic car agent, the car agent will park and start charging and notify the MSA when min charge has 
 //been hit (this will hopefully be more useful later). When max charge is reached the MSA is notified, charging will stop 
@@ -10,7 +15,40 @@ public class CarAgent extends Agent {
 	double minCharge = 0.5;
 	double maxCharge = 1;
 	
-	//another function for what the car does before getting spot 
+	protected void setup() {
+		//First set-up message receiving behaviour
+		CyclicBehaviour messageListeningBehaviour = new CyclicBehaviour(this)	
+				{
+					public void action() {
+						ACLMessage msg = receive();
+						if (msg != null) {
+							System.out.println(getLocalName() + ": Received response" + msg.getContent() + " from " + msg.getSender().getLocalName());
+						}
+						block();
+					}
+				};
+				addBehaviour(messageListeningBehaviour);
+				
+				//Send request to MSAAgent schedule spot for now 
+				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+				msg.setContent("Request Schedule");
+				msg.addReceiver(new AID("MSAAgent", AID.ISLOCALNAME));
+			
+			//Send Message (only once)
+			System.out.println(getLocalName() + ": Sending message " + msg.getContent() + " to ");
+			Iterator receivers = msg.getAllIntendedReceiver();
+			while(receivers.hasNext()) {
+				System.out.println(((AID)receivers.next()).getLocalName());
+			}
+			send(msg);
+				
+	}
+	
+	//function for request of scheduling  
+	/**
+	 * Car requests schedule spot now - add in time request later 
+	 * msa reponds with yes (you can charge now) or no and agent dies
+	 */
 	
 	
 	protected void chargeCar() {
