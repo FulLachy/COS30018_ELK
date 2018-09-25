54223c1f47d65e5a8e7235cc6f5caeec5d9d49cc
Elizabeth <elizabeth.wain@hotmail.com>
2018-09-25 14:14:53 +1000

    Car agent update - 'finished' car agent, added carInformation class

---
+++ Project/src/CarInformation/CarInformation.java
@@ -0,0 +1,19 @@
+package CarInformation;
+
+//import car stuff some how
+/*
+ * This class contains the details of the car that are needed by the MSA for scheduling
+ */
+
+public class CarInformation {
+	public CarAgent type = CarAgent.values
+	public int carId;
+	public double reqStartTime = 0; //preferred start time to start charging from car 
+	public double reqFinishTime = 0; //time car needs to be finished by 
+	public double reqMinCharge = 0; //minimum charge for car to have before it can leave
+	
+	public carMessage {
+		
+	}
+
+}
