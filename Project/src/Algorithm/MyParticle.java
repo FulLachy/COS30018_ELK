fb3d509c215bab6eb6aa3437ae51ec4563116371
Elizabeth <elizabeth.wain@hotmail.com>
2018-11-01 15:20:44 +1100

    PSO update

--- Project/src/Algorithm/MyParticle.java
+++ Project/src/Algorithm/MyParticle.java
@@ -3,9 +3,12 @@ package Algorithm;
 import net.sourceforge.jswarm_pso.Particle;
 
 public class MyParticle extends Particle {
-	//Create a 2-dimensional particle 
+	public static int NUMBER_OF_DIMENSIONS = 2; //number of particle dimensions
+	double particleData;
+	
 	public MyParticle() {
-		super(1);
+		super(NUMBER_OF_DIMENSIONS); //creates a 2-dimensional particle
+		//particle data
 	}
 }
 
